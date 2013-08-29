4a55b3d98ff07a1c281d8e2c554a97124b7c1c9f
M1chael <asoi@mail.ru>
2013-08-29 23:07:17 +0600

    0.5 sec for 1 page

--- JCounter.rb
+++ JCounter.rb
@@ -11,14 +11,25 @@ class JCounter
 	include Logging
 
 	def initialize
-		for i in 1..1
+		logger.info('---====== S T A R T E D =====---')
+		wdays = Array.new(7, 0)
+		@date = ''
+		for i in 1..10240
 			uri = URI("http://api.juick.com/messages?page=#{i}")
 			res = Net::HTTP.get_response(uri)
-			JSON.parse(res.body).each {|msg|
+			JSON.parse(res.body.gsub('	', ' ')).each {|msg|
 				dcTime = Time.parse(msg['timestamp']) + 4 * 60 * 60 # Default City Time
-				puts "#{dcTime.wday}"
+				wdays[dcTime.wday - 1] += 1
+				if i == 10240
+					@date = "#{dcTime.day}.#{dcTime.month}.#{dcTime.year}"
+				end
 			}
+			if i % 1000 == 0
+				logger.info(i)
+			end		
 		end
+		logger.info(@date)
+		logger.info(wdays.inspect)
 	end
 
 end
