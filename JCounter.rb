0331800410a48ee663c4e608f479afbf01fcb071
M1chael <asoi@mail.ru>
2013-08-31 13:24:43 +0600

    log cleared, 131072 pages for parsing

--- JCounter.rb
+++ JCounter.rb
@@ -14,22 +14,20 @@ class JCounter
 		logger.info('---====== S T A R T E D =====---')
 		wdays = Array.new(7, 0)
 		@date = ''
-		for i in 1..10240
+		for i in 1..131072
 			uri = URI("http://api.juick.com/messages?page=#{i}")
 			res = Net::HTTP.get_response(uri)
 			JSON.parse(res.body.gsub('	', ' ')).each {|msg|
 				dcTime = Time.parse(msg['timestamp']) + 4 * 60 * 60 # Default City Time
 				wdays[dcTime.wday - 1] += 1
-				if i == 10240
+				if i % 2048 == 0
 					@date = "#{dcTime.day}.#{dcTime.month}.#{dcTime.year}"
 				end
 			}
-			if i % 1000 == 0
-				logger.info(i)
+			if i % 2048 == 0
+				logger.info("#{i} pages, #{@date}, #{wdays.inspect}")
 			end		
 		end
-		logger.info(@date)
-		logger.info(wdays.inspect)
 	end
 
 end
